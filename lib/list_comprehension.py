7ce329fb567cd314969e035cd4b186da1cd8b2e6
samuel ndegwa <138137867+Ndegwa2@users.noreply.github.com>
2024-09-09 06:41:28 +0300

    Update list_comprehension.py

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    """
+    Returns a list of all even elements from the given list of integers.
+    """
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    """
+    Takes a list of sentence strings and returns a list of sentence strings
+    with exclamation marks added to the end.
+    """
+    return [sentence + '!' for sentence in sentence_list]
